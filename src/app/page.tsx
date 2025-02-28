0fac700a94ac4a5baa182886ff639a6ee6194fcd
Howard Yeend <howard.yeend@os.uk>
2025-02-28 16:59:00 +0000

    feat: preview a local image in a canvas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,68 @@
-import Image from "next/image";
+'use client';
 import styles from "./page.module.css";
+import {useEffect, useRef, useState} from "react";
+
+const ImagePreview = ({image}) => {
+    const ref = useRef(null);
+    const maxWidth = 512;
+    const maxHeight = 512;
+
+    useEffect(() => {
+        const canvas = ref.current;
+        const ctx = canvas.getContext('2d');
+
+        // Clamp the size
+        if (image.width > maxWidth || image.height > maxHeight) {
+            const scale = Math.min(maxWidth / image.width, maxHeight / image.height);
+            canvas.width = image.width * scale;
+            canvas.height = image.height * scale;
+        } else {
+            canvas.width = image.width;
+            canvas.height = image.height;
+        }
+
+        // Clear previous image
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        //ctx.drawImage(image,0,0);
+        // scale image to canvas size:
+        const scale = Math.min(canvas.width / image.width, canvas.height / image.height);
+        ctx.drawImage(image, 0, 0, image.width * scale, image.height * scale);
+
+    }, [image]);
+
+    return <canvas className={styles.imagePreview} width={maxWidth} height={maxHeight} ref={ref}></canvas>;
+}
 
 export default function Home() {
+
+    const [image, setImage] = useState(null);
+
+    const handleImageChange = (e) => {
+        // On image input change, read the file data and store it in state.
+        const file = e.target.files[0];
+        const reader = new FileReader();
+        reader.onload = (e) => {
+            const img = new Image();
+            img.onload = () => {
+                setImage(img);
+            }
+            img.src = e.target.result as string;
+        }
+        reader.readAsDataURL(file);
+    }
+
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
+        <h1>Prism Image Analyser</h1>
+          <label>
+              <div>Pick an image and we will analyse it.</div>
+              <br />
+              <input type='file' onChange={handleImageChange} />
+          </label>
+          {image && <ImagePreview image={image} />}
+          <aside>The image never leaves your machine, all processing is done locally.</aside>
       </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
